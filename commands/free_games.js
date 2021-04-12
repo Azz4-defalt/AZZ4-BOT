20365d8814976a86b340fdd117a43ea608431236
azz4_defalt <82065706+Azz4-defalt@users.noreply.github.com>
2021-04-12 08:19:57 -0300

    Update free_games.js

--- commands/free_games.js
+++ commands/free_games.js
@@ -1,3 +1,3 @@
 module.exports = (client,msg) => {
-    msg.channel.send("[Epic Games Store] SAMURAI SHODOWN NEOGEO COLLECTION \nSAMURAI SHODOWN NEOGEO COLLECTION esta gratuito ate o dia 18/06\nTags:Luta, Multijogador, Arcadce\nPlataforas:PC's \nhhttps://www.epicgames.com/store/pt-BR/product/samurai-shodown-neogeo-collection/home\n\n[Epic Games Store] ARK: Survival Evolved \nARK: Survival Evolved esta gratuito ate o dia 18/06\nTags:Sobrevivência,RPG,Aventura\nPlataforas:PC's \nhhttps://https://www.epicgames.com/store/pt-BR/product/ark/home")
+    msg.channel.send("Digite aqui o jogo que esta gratuito")
 }
