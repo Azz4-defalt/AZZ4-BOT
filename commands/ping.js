1c47d78b2eb0cee1ea819de3a88d7fbbe36c40c8
Azz4-defalt <azz4.defalt@gmail.com>
2021-04-13 08:07:29 -0300

    Adiçao de comando de latencia

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 module.exports = (client,msg) => {
-    msg.channel.send("Pong!!!")
-}
+    msg.channel.send(`🏓O bot esta com a latencia de ${Date.now() - msg.createdTimestamp}ms. A latencia da API é ${Math.round(client.ws.ping)}ms`)
+    }
